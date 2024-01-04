afa05ca9329acd2dcf2321645d286289c697f3f8
sanskarsri18 <99880907+sanskarsri18@users.noreply.github.com>
2024-01-04 20:15:52 +0530

    Create Find_the_duplicate_in_an_array_of_N+1_integers.cpp

---
+++ Find_the_duplicate_in_an_array_of_N+1_integers.cpp
@@ -0,0 +1,19 @@
+Solution 1
+
+class Solution {
+public:
+    int findDuplicate(vector<int>& nums) {
+        int n = nums.size();
+        sort(nums.begin(), nums.end());
+        for(int i = 0; i < n; i++){
+            if(nums[i] == nums[i+1]){
+                return nums[i];
+            }
+        }
+        return -1;
+    }
+};
+
+
+Solution 2
+
