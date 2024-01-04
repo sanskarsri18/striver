978cca431129783fa4dfaf5843600d262f06d35c
sanskarsri18 <99880907+sanskarsri18@users.noreply.github.com>
2024-01-04 21:32:22 +0530

    Update Find_the_duplicate_in_an_array_of_N+1_integers.cpp

--- Find_the_duplicate_in_an_array_of_N+1_integers.cpp
+++ Find_the_duplicate_in_an_array_of_N+1_integers.cpp
@@ -17,3 +17,45 @@ public:
 
 Solution 2
 
+class Solution {
+public:
+    int findDuplicate(vector<int>& nums) {
+        int n = nums.size();
+        int *freq = new int[n];
+        for(int i = 0; i < n; i++){
+            freq[i] = 0;
+        }
+        for(int i = 0; i < n; i++){
+            if(freq[nums[i]] == 1){
+                return nums[i];
+            }
+            freq[nums[i]]++;
+        }
+        return -1;
+    }
+};
+
+
+Solution 3
+
+class Solution {
+public:
+    int findDuplicate(vector<int>& nums) {
+        int n = nums.size();
+        int slow = nums[0];
+        int fast = nums[0];
+
+        do{
+            slow = nums[slow];
+            fast = nums[nums[fast]];
+        } while(slow != fast);
+
+        fast = nums[0];
+        while(slow != fast){
+            slow = nums[slow];
+            fast = nums[fast];
+        }
+        return fast;
+        
+    }
+};
