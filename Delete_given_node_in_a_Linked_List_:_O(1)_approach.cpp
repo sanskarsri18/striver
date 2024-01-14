aa95c7b32c716b36d488e9fb9be1c7d5cb7ccf87
sanskarsri18 <99880907+sanskarsri18@users.noreply.github.com>
2024-01-14 16:28:37 +0530

    Create Delete_given_node_in_a_Linked_List_:_O(1)_approach.cpp

---
+++ Delete_given_node_in_a_Linked_List_:_O(1)_approach.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    void deleteNode(ListNode* node) {
+        if(node -> next == NULL){
+            node = NULL;
+        }
+        node -> val = node -> next -> val;
+        node -> next = node -> next -> next;
+    }
+};
