9d1c453447743407b6f86525081f6c597ad669f1
sanskarsri18 <99880907+sanskarsri18@users.noreply.github.com>
2024-01-29 23:21:33 +0530

    Update N_Queen_Problem_|_Return_all_Distinct_Solutions_to_the_N-Queens_Puzzle.cpp

--- N_Queen_Problem_|_Return_all_Distinct_Solutions_to_the_N-Queens_Puzzle.cpp
+++ N_Queen_Problem_|_Return_all_Distinct_Solutions_to_the_N-Queens_Puzzle.cpp
@@ -67,3 +67,42 @@ public:
 
 Optimal Approach
 
+
+class Solution {
+public:
+    void solve(vector<vector<string>>& ans, vector<string> board, vector<int> leftRow, vector<int> upperDiagonal, vector<int> LowerDiagonal, int n, int col){
+        if(col == n){
+            ans.push_back(board);
+            return ;
+        }
+
+        for(int row = 0; row < n; row++){
+            if(leftRow[row] == 0 && upperDiagonal[n-1+col-row] == 0 && LowerDiagonal[row + col] == 0){
+                leftRow[row] = 1;
+                upperDiagonal[n-1+col-row] = 1;
+                LowerDiagonal[col+row] = 1;
+                board[row][col] = 'Q';
+                solve(ans , board, leftRow, upperDiagonal, LowerDiagonal, n, col + 1);
+                leftRow[row] = 0;
+                upperDiagonal[n-1+col-row] = 0;
+                LowerDiagonal[col+row] = 0;
+                board[row][col] = '.';
+            }
+        }
+    }
+
+    vector<vector<string>> solveNQueens(int n) {
+        vector<vector<string>> ans;
+        vector<string> board (n);
+        string s(n , '.');
+        for(int i = 0; i < n; i++){
+            board[i] = s;
+        }
+        vector<int> leftRow(n , 0);
+        vector<int> upperDiagonal(2*n-1 , 0);
+        vector<int> LowerDiagonal(2*n-1 , 0);
+        solve(ans , board, leftRow, upperDiagonal, LowerDiagonal, n, 0);
+        return ans;
+    }
+};
+
